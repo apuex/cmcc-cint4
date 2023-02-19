ee86c2e05cc422a7a1e7ba671a4ae57fdaebee7c
Wangxy <xtwxy@hotmail.com>
2023-02-20 00:45:58 +0800

    implement a buggy CRC16 algorithm from 3.5.

--- message/src/main/java/com/github/apuex/cmcc/cint4/Util.java
+++ message/src/main/java/com/github/apuex/cmcc/cint4/Util.java
@@ -9,6 +9,11 @@ import java.nio.charset.Charset;
 public class Util {
     static public final Charset charset = Charset.forName("GB18030");
     static public final byte padding = 0x20;
+    static public final short CRC16_INITIAL_VALUE = (short)0xFFFF;
+    //static public final short CRC16_POLYNOMIAL = (short)0xA001;
+    //static public final short CRC16_INITIAL_VALUE = (short)0x0000;
+    static public final short CRC16_POLYNOMIAL = (short)0x1021;
+    static public final boolean CRC16_REVERSE_BITS = false;
 
     static public void encodeString(ByteBuffer buf, String s, int maxLength) {
         byte[] ba = s.getBytes(charset);
@@ -29,8 +34,46 @@ public class Util {
 	return s.trim();
     }
 
-    static public short CRC16(byte[] ba, int begin, int end) {
-	return (short)0xCAFE;
-    }
+    /* CRC16 checksum */
+public static byte reverseBits(byte b) {
+  return (byte)((b & 0x01) << 7
+         |(b & 0x02) << 5
+         |(b & 0x04) << 3
+         |(b & 0x08) << 1
+         |(b & 0x10) >>> 1
+         |(b & 0x20) >>> 3
+         |(b & 0x40) >>> 5
+         |(b & 0x80) >>> 7
+         );
+}
+
+public static short newCRC16(short crc16, boolean rev, byte b) {
+  short bp = rev ? reverseBits(b) : b;
+  return (short)(crc16 ^ (bp << 8));
+}
+
+public static short CRC16(short poly, boolean rev, short crc16, byte b) {
+  short crc16p = newCRC16(crc16, rev, b);
+  for(int i = 0; i < 8; ++i) {
+    crc16p = (0x0000 != (crc16p & 0x8000))
+           ? (short)((crc16p << 1) ^ poly)
+           : (short)(crc16p << 1);
+  }
+  return crc16p;
+}
+
+public static short CRC16(short crc16, byte b) {
+  return CRC16(CRC16_POLYNOMIAL, CRC16_REVERSE_BITS, crc16, b);
+}
+
+public static short CRC16(byte[] ba, int begin, int end)
+{
+  short crc16 = CRC16_INITIAL_VALUE;
+  for(int i = begin; i != end; ++i) {
+    crc16 = CRC16(crc16, ba[i]);
+  }
+  return crc16;
+}
 
 }
+
